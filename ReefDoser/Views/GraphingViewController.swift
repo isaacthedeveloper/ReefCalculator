c02d76b3ab0d9013e8b772f36e83bfbb0bd51fa3
Isaac <Isaacthedeveloper@gmail.com>
2019-10-28 16:20:34 -0400

    Add graphs and counters - MAJOR ISSUE WITH UI

--- ReefDoser/Views/GraphingViewController.swift
+++ ReefDoser/Views/GraphingViewController.swift
@@ -10,7 +10,23 @@ import UIKit
 class GraphingViewController: UIViewController {
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+    @IBOutlet weak var average: UILabel!
+    @IBOutlet weak var max: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    var isGraphShowing = false
     
+    @IBAction func counterViewTap(_ gesture: UITapGestureRecognizer?) {
+        if (isGraphShowing) {
+            // Hide the graph
+            UIView.transition(from: graphView, to: counterView, duration: 1.0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        } else {
+            // Show the graph
+            UIView.transition(from: counterView, to: graphView, duration: 1.0, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
+        }
+        isGraphShowing = !isGraphShowing
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         counterLabel.text = String(counterView.counter)
@@ -24,5 +40,9 @@ class GraphingViewController: UIViewController {
             }
         }
         counterLabel.text = String(counterView.counter)
+        
+        if isGraphShowing {
+            counterViewTap(nil)
+        }
     }
 }
